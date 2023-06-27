91d5de4c8f75a0da53449c6ce47ab162cc351d6b
luca.canali <canali.luca998@gmail.com>
2023-06-27 22:40:11 +0200

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -1,17 +1,25 @@
 import pyautogui
 import time
+import keyboard
 
 def autoclicker(interval, duration):
     end_time = time.time() + duration
     while time.time() < end_time:
-    #while True:
+        if keyboard.is_pressed('F6'):
+            break
         pyautogui.click()
         time.sleep(interval)
 
+    print("Termino")
+
 def autoclicker():
     while True:
+        if keyboard.is_pressed('F6'):
+            break
         pyautogui.click()
         time.sleep(interval)
+        
+    print("Termino")
 
 interval = 0.5  
 duration = 10  

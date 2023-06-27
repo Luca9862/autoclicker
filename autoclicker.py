a9f0bcc144d28d64a45b38dc5cba5ad70d2af654
luca.canali <canali.luca998@gmail.com>
2023-06-27 22:50:45 +0200

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -1,9 +1,16 @@
 import pyautogui
 import time
 import keyboard
+import os
+
+def check_admin_privileges():
+    return os.getuid() == 0
 
 def autoclicker(interval, duration):
     end_time = time.time() + duration
+    if not check_admin_privileges():
+        print("Termino, non ho i permessi")
+        return
     while time.time() < end_time:
         if keyboard.is_pressed('F6'):
             break
@@ -13,6 +20,9 @@ def autoclicker(interval, duration):
     print("Termino")
 
 def autoclicker():
+    if not check_admin_privileges():
+        print("Termino, non ho i permessi")
+        return
     while True:
         if keyboard.is_pressed('F6'):
             break

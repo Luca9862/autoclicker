8870f3d858cd2f1c0dffb7922a1b2c9527c220e0
luca.canali <canali.luca998@gmail.com>
2023-07-14 12:12:10 +0200

    Add GUI

--- autoclicker.py
+++ autoclicker.py
@@ -2,6 +2,15 @@ import pyautogui
 import time
 import keyboard
 import os
+import sys
+import tkinter as tk
+
+def run_as_admin():
+    if os.geteuid() != 0:
+        # Richiede l'autenticazione dell'utente come amministratore
+        command = 'do shell script "{}" with administrator privileges'.format(' '.join(sys.argv))
+        os.system('/usr/bin/osascript -e \'{}\''.format(command))
+        sys.exit(0)
 
 def check_admin_privileges():
     return os.getuid() == 0
@@ -19,18 +28,24 @@ def autoclicker(interval, duration):
 
     print("Return")
 
-def autoclicker():
+def autoclickerNoDuration(interval):
     if not check_admin_privileges():
         print("The program requires administrator permissions to be executed")
         return
-    while True:
-        if keyboard.is_pressed('F6'):
-            break
+    while not keyboard.is_pressed("F6"):
+        #if keyboard.is_pressed('F6'):
+            #break
         pyautogui.click()
         time.sleep(interval)
-        
+
     print("Return")
 
-interval = 0.5  
-duration = 10  
-autoclicker()
+'''interval = 0.6 
+run_as_admin()
+autoclickerNoDuration(interval)'''
+# Crea la finestra principale
+root = tk.Tk()
+root.title("AUTOCLICKER by Luca Canali")
+root.geometry("350x200")
+root.mainloop()
+
